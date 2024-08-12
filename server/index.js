d3c573ac7bf485cf90ae975e2246e05edfb0d836
Femi Olayode <ofemiracheal95@gmail.com>
2024-08-12 13:51:51 -0600

    adding cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require("express");
+
+const fs = require("fs"); // to read and write file
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+
 const app = express();
-const fs = require('fs');// to read and write file
-const path = require('path');
-const bodyParser = require('body-parser');
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+app.use(cors());
 
 const port = 4000;
-const dataFile = path.join(__dirname, 'todoList.json');
+const dataFile = path.join(__dirname, "todoList.json");
 
 //Middleware
 app.use(bodyParser.json());
@@ -13,7 +23,7 @@ app.use(bodyParser.json());
 // Function to read tasks from file
 const readTasksFromFile = () => {
   try {
-    const data = fs.readFileSync(dataFile, 'utf8');
+    const data = fs.readFileSync(dataFile, "utf8");
     return JSON.parse(data);
   } catch (err) {
     return [];
@@ -22,7 +32,7 @@ const readTasksFromFile = () => {
 
 // Function to write tasks to file
 const writeTasksToFile = (tasks) => {
-  fs.writeFileSync(dataFile, JSON.stringify(tasks, null, 2), 'utf8');
+  fs.writeFileSync(dataFile, JSON.stringify(tasks, null, 2), "utf8");
 };
 
 // Routes
@@ -30,42 +40,42 @@ app.get("/", (req, res) => {
   res.send("Hello To Do");
 });
 //to get to do list
-app.get('/todos', (req, res) => {
-    const todolist = readTasksFromFile();
-    try {
-      if (todolist.length === 0) {
-        res.status(204).json({ message: 'List is empty', todos: [] });
-      } else {
-        res.status(200).json(todolist);
-      }
-    } catch (err) {
-      res.status(500).json({ message: 'Error reading the list' });
-    }
-  });
-  
-// to get list items with list ID
-app.get('/todos/:id', (req, res) => {
-    const todolist = readTasksFromFile();
-    const todoId = parseInt(req.params.id);
-    const todo = todolist.find(t => t.id === todoId);
-  
-    if (todo) {
-      if (todo.todoItems && todo.todoItems.length === 0) {
-         res.status(204).send('List is empty'); 
-      } else {
-        res.json(todo);
-      }
+app.get("/todos", (req, res) => {
+  const todolist = readTasksFromFile();
+  try {
+    if (todolist.length === 0) {
+      res.status(204).json({ message: "List is empty", todos: [] });
     } else {
-      res.status(404).send('Todo not found');
+      res.status(200).json(todolist);
     }
-  });
-  
+  } catch (err) {
+    res.status(500).json({ message: "Error reading the list" });
+  }
+});
+
+// to get list items with list ID
+app.get("/todos/:id", (req, res) => {
+  const todolist = readTasksFromFile();
+  const todoId = parseInt(req.params.id);
+  const todo = todolist.find((t) => t.id === todoId);
+
+  if (todo) {
+    if (todo.todoItems && todo.todoItems.length === 0) {
+      res.status(204).send("List is empty");
+    } else {
+      res.json(todo);
+    }
+  } else {
+    res.status(404).send("Todo not found");
+  }
+});
+
 // to post lists to server
-app.post('/todos', (req, res) => {
+app.post("/todos", (req, res) => {
   const newTodo = req.body;
   const todolist = readTasksFromFile();
   if (!newTodo.id || !newTodo.todoItems || !newTodo.title) {
-    return res.status(400).send('Invalid todo data');
+    return res.status(400).send("Invalid todo data");
   }
   todolist.push(newTodo);
   writeTasksToFile(todolist);
@@ -73,96 +83,104 @@ app.post('/todos', (req, res) => {
 });
 
 // to post list items with given list ID
-app.post('/todos/:id/todoItems', (req, res) => {
+app.post("/todos/:id/todoItems", (req, res) => {
   const newItem = req.body;
   const todoId = parseInt(req.params.id, 10);
   const todolist = readTasksFromFile();
-  const list = todolist.find(item => item.id === todoId);
-  if (!newItem.id || !newItem.date || !newItem.title || newItem.isCompleted === undefined) {
-    return res.status(400).send('Invalid todo data');
+  const list = todolist.find((item) => item.id === todoId);
+  if (
+    !newItem.id ||
+    !newItem.date ||
+    !newItem.title ||
+    newItem.isCompleted === undefined
+  ) {
+    return res.status(400).send("Invalid todo data");
   }
 
   if (list) {
     list.todoItems.push(newItem);
     writeTasksToFile(todolist);
-    return res.status(201).send('Item added successfully');
+    return res.status(201).send("Item added successfully");
   } else {
-    return res.status(404).send('List not found');
+    return res.status(404).send("List not found");
   }
 });
 
 // to update list title with given ID
-app.put('/todos/:id', (req, res) => {
+app.put("/todos/:id", (req, res) => {
   const todoId = parseInt(req.params.id, 10);
   const updatedTodo = req.body;
   const todolist = readTasksFromFile();
-  const todoIndex = todolist.findIndex(t => t.id === todoId);
+  const todoIndex = todolist.findIndex((t) => t.id === todoId);
 
   if (todoIndex !== -1) {
     todolist[todoIndex] = { ...todolist[todoIndex], ...updatedTodo };
     writeTasksToFile(todolist);
     res.json(todolist[todoIndex]);
   } else {
-    res.status(404).send('Todo not found');
+    res.status(404).send("Todo not found");
   }
 });
 // to update list item title with the with item id and list id
-app.put('/todos/:id/todoItems/:itemId', (req, res) => {
+app.put("/todos/:id/todoItems/:itemId", (req, res) => {
   const todoId = parseInt(req.params.id, 10);
   const itemId = parseInt(req.params.itemId, 10);
   const updatedItem = req.body;
   const todolist = readTasksFromFile();
 
-  const list = todolist.find(item => item.id === todoId);
+  const list = todolist.find((item) => item.id === todoId);
 
   if (list) {
-    const itemIndex = list.todoItems.findIndex(item => item.id === itemId);
+    const itemIndex = list.todoItems.findIndex((item) => item.id === itemId);
 
     if (itemIndex !== -1) {
-      list.todoItems[itemIndex] = { ...list.todoItems[itemIndex], ...updatedItem };
+      list.todoItems[itemIndex] = {
+        ...list.todoItems[itemIndex],
+        ...updatedItem,
+      };
       writeTasksToFile(todolist);
       return res.json(list.todoItems[itemIndex]);
     } else {
-      return res.status(404).send('Todo item not found');
+      return res.status(404).send("Todo item not found");
     }
   } else {
-    return res.status(404).send('List not found');
+    return res.status(404).send("List not found");
   }
 });
 // to delete list item with item id and list id
-app.delete('/todos/:id/todoItems/:itemId', (req, res) => {
+app.delete("/todos/:id/todoItems/:itemId", (req, res) => {
   const todoId = parseInt(req.params.id, 10);
   const itemId = parseInt(req.params.itemId, 10);
   const todolist = readTasksFromFile();
 
-  const list = todolist.find(item => item.id === todoId);
+  const list = todolist.find((item) => item.id === todoId);
 
   if (list) {
-    const itemIndex = list.todoItems.findIndex(item => item.id === itemId);
+    const itemIndex = list.todoItems.findIndex((item) => item.id === itemId);
 
     if (itemIndex !== -1) {
       list.todoItems.splice(itemIndex, 1);
       writeTasksToFile(todolist);
       return res.status(204).send();
     } else {
-      return res.status(404).send('Todo item not found');
+      return res.status(404).send("Todo item not found");
     }
   } else {
-    return res.status(404).send('List not found');
+    return res.status(404).send("List not found");
   }
 });
 // to delete list with given id
-app.delete('/todos/:id', (req, res) => {
+app.delete("/todos/:id", (req, res) => {
   const todoId = parseInt(req.params.id, 10);
   const todolist = readTasksFromFile();
-  const index = todolist.findIndex(t => t.id === todoId);
+  const index = todolist.findIndex((t) => t.id === todoId);
 
   if (index !== -1) {
     todolist.splice(index, 1);
     writeTasksToFile(todolist);
     res.status(204).send();
   } else {
-    res.status(404).send('Todo not found');
+    res.status(404).send("Todo not found");
   }
 });
 
--- todoListClient/to-do/src/Componants/Todo/TodoList.js
+++ todoListClient/to-do/src/Componants/Todo/TodoList.js
@@ -1,163 +1,188 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Tooltip } from 'react-tooltip';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Tooltip } from "react-tooltip";
 import { MdPlaylistAdd } from "react-icons/md";
 import { IoIosClose } from "react-icons/io";
 import { GrEdit } from "react-icons/gr";
-import './TodoList.css';
-import TodoListItems from '../ToDoItems/TodoListItems';
-import AddTodoList from '../AddTodo/AddTodoList'; // Import the new component
-
+import "./TodoList.css";
+import TodoListItems from "../ToDoItems/TodoListItems";
+import AddTodoList from "../AddTodo/AddTodoList"; // Import the new component
 
 export default function Todolist() {
-    const [listitems, setListItems] = useState([]); // State to hold all lists
-    const [messageList, setMessageList] = useState('');  //State to set message after delete, add, update
-    const [isEditing, setIsEditing] = useState(false); //to check we editing the title
-    const [editingId, setEditingId] = useState(null);  //to check edit
-    const [editingValue, setEditingValue] = useState(''); 
-    const [selectedListId, setSelectedListId] = useState(null); 
-    const [selectedListTitle, setSelectedListTitle] = useState(null);  // State to hold the selected list Title
-    const [fromAdd, setFromAdd] = useState(false);
-    // function to call on first load
-    useEffect(() => {
-        fetchTodos();
-    }, []);
-     // to fetch all Lists
-    const fetchTodos = async () => {
-        try {
-            const res = await axios.get('/todos');
-            if (res.status === 200) {
-                setMessageList('');
-                setListItems(res.data); 
-                if (res.data.length > 0) {
-                    handleSelectList(res.data[0].id, res.data[0].title);
-                    console.log(res.data[0].id, res.data[0].title)
-                }
-            } else if (res.status === 204) {
-                setMessageList('Lists are empty');
-                setListItems([]); 
-            }
-        } catch (error) {
-            if (error.response && error.response.status === 500) {
-                setMessageList('Error reading the list');
-            } else {
-                setMessageList('Error fetching todo items');
-            }
-        }
-    };
+  const [listitems, setListItems] = useState([]); // State to hold all lists
+  const [messageList, setMessageList] = useState(""); //State to set message after delete, add, update
+  const [isEditing, setIsEditing] = useState(false); //to check we editing the title
+  const [editingId, setEditingId] = useState(null); //to check edit
+  const [editingValue, setEditingValue] = useState("");
+  const [selectedListId, setSelectedListId] = useState(null);
+  const [selectedListTitle, setSelectedListTitle] = useState(null); // State to hold the selected list Title
+  const [fromAdd, setFromAdd] = useState(false);
+  // const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+  // function to call on first load
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+  // to fetch all Lists
+  const fetchTodos = async () => {
+    try {
+      // console.log('backendUrl',backendUrl);
+      const res = await axios.get("https://vercelfs-server.vercel.app/todos");
+      if (res.status === 200) {
+        console.log("res is gotten");
+        setMessageList("");
+        setListItems(res.data);
 
-        // to post item to server
-    const postTodoItem = async (addListValue) => {
-        try {
-            const res = await axios.post('/todos', {
-                id: new Date().getTime(),
-                title: addListValue,
-                todoItems: [],
-                date: new Date().toISOString().split('T')[0],
-            });
-            setMessageList(res.data.message);
-            setFromAdd(true);
-            const newList = res.data;
-            await fetchTodos(); 
-             
-            handleSelectList( newList.id, newList.title);
-           
-        } catch (error) {
-            setMessageList('Error posting todo item');
+        if (res.data.length > 0) {
+          handleSelectList(res.data[0].id, res.data[0].title);
         }
-    };
- //to add all functions in one call for posting list
-    const handleAddItemToServer = (addListValue) => {
-        if (addListValue.trim()) {
-            postTodoItem(addListValue); 
-            setMessageList('List Added');
-        }
-    };
+      } else if (res.status === 204) {
+        setMessageList("Lists are empty");
+        setListItems([]);
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 500) {
+        setMessageList("Error reading the list");
+      } else {
+        setMessageList("Error fetching todo items");
+      }
+    }
+  };
+
+  // to post item to server
+  const postTodoItem = async (addListValue) => {
+    try {
+      const res = await axios.post("/todos", {
+        id: new Date().getTime(),
+        title: addListValue,
+        todoItems: [],
+        date: new Date().toISOString().split("T")[0],
+      });
+      setMessageList(res.data.message);
+      setFromAdd(true);
+      const newList = res.data;
+      await fetchTodos();
+
+      handleSelectList(newList.id, newList.title);
+    } catch (error) {
+      setMessageList("Error posting todo item");
+    }
+  };
+  //to add all functions in one call for posting list
+  const handleAddItemToServer = (addListValue) => {
+    if (addListValue.trim()) {
+      postTodoItem(addListValue);
+      setMessageList("List Added");
+    }
+  };
   // delete list from server
-    const handleRemoveItem = async (index) => {
-        try {
-            const res = await axios.delete(`/todos/${index}`);
-            setMessageList('List Removed');
-            fetchTodos(); 
-            window.location.reload()
-        } catch (error) {
-            setMessageList('Error deleting todo item');
-        }
-    };
- // To update the list title
-    const handleEditItem = (item) => {
-        setIsEditing(true);
-        setEditingId(item.id);
-        setEditingValue(item.title);
-    };
-//to save edit value to server
-    const handleSaveEdit = async () => {
-        try {
-            const res = await axios.put(`/todos/${editingId}`, {
-                title: editingValue
-            });
-            setMessageList('Todo updated successfully');
-            setIsEditing(false);
-            setEditingId(null);
-            fetchTodos();
-        } catch (error) {
-            setMessageList('Error updating todo item');
-        }
-    };
+  const handleRemoveItem = async (index) => {
+    try {
+      const res = await axios.delete(`/todos/${index}`);
+      setMessageList("List Removed");
+      fetchTodos();
+      window.location.reload();
+    } catch (error) {
+      setMessageList("Error deleting todo item");
+    }
+  };
+  // To update the list title
+  const handleEditItem = (item) => {
+    setIsEditing(true);
+    setEditingId(item.id);
+    setEditingValue(item.title);
+  };
+  //to save edit value to server
+  const handleSaveEdit = async () => {
+    try {
+      const res = await axios.put(`/todos/${editingId}`, {
+        title: editingValue,
+      });
+      setMessageList("Todo updated successfully");
+      setIsEditing(false);
+      setEditingId(null);
+      fetchTodos();
+    } catch (error) {
+      setMessageList("Error updating todo item");
+    }
+  };
 
-    const handleSelectList = (id, title) => {
-        setSelectedListId(id);
-        setSelectedListTitle(title);
-    };
- // call function to add list with enter
-    const handleEditValue = (e) => {
-        if (e.key === 'Enter') {
-            handleSaveEdit();
-        }
-    };
+  const handleSelectList = (id, title) => {
+    setSelectedListId(id);
+    setSelectedListTitle(title);
+  };
+  // call function to add list with enter
+  const handleEditValue = (e) => {
+    if (e.key === "Enter") {
+      handleSaveEdit();
+    }
+  };
 
-    return (
-        <div className='container'>
-            <div className='leftsection'>
-                <div className='todoListContainer'>
-                    <div className='todolist'>
-                        <Tooltip id='tip' style={{ color: "#cdc4df", backgroundColor: '#78659e', borderRadius: '10px' }} />
-                        <h1 style={{ color: "#78659e" }}>Lists</h1>
-                        <AddTodoList onAddItem={handleAddItemToServer} /> 
-                        <ul className="listItems">
-                            {listitems.map((listitem) => (
-                                <li className='listItem' key={listitem.id} onClick={() => handleSelectList(listitem.id, listitem.title)}
-                                    data-tooltip-id="tip"
-                                    data-tooltip-content="Click to add to-do"
-                                    data-tooltip-place="left-start"
-                                >
-                                    {isEditing && editingId === listitem.id ? (
-                                        <input
-                                            type="text"
-                                            className='editInput'
-                                            value={editingValue}
-                                            onChange={(e) => setEditingValue(e.target.value)}
-                                            onKeyDown={handleEditValue}
-                                        />
-                                    ) : (
-                                        <p>{listitem.title}</p>
-                                    )}
-                                    {isEditing && editingId === listitem.id ? (
-                                        <MdPlaylistAdd className='addItemBtn' onClick={handleSaveEdit} />
-                                    ) : (
-                                        <GrEdit className='editBtn' onClick={() => handleEditItem(listitem)} />
-                                    )}
-                                    <IoIosClose className='deleteListItem' onClick={() => handleRemoveItem(listitem.id)} />
-                                </li>
-                            ))}
-                        </ul>
-                        {messageList && <p className="message">{messageList}</p>}
-                    </div>
-                </div>
-            </div>
-            <div className='rghtSec'>
-                {selectedListId && <TodoListItems listId={selectedListId} listTitle={selectedListTitle} />} 
-            </div>
+  return (
+    <div className="container">
+      <div className="leftsection">
+        <div className="todoListContainer">
+          <div className="todolist">
+            <Tooltip
+              id="tip"
+              style={{
+                color: "#cdc4df",
+                backgroundColor: "#78659e",
+                borderRadius: "10px",
+              }}
+            />
+            <h1 style={{ color: "#78659e" }}>Lists</h1>
+            <AddTodoList onAddItem={handleAddItemToServer} />
+            <ul className="listItems">
+              {listitems.map((listitem) => (
+                <li
+                  className="listItem"
+                  key={listitem.id}
+                  onClick={() => handleSelectList(listitem.id, listitem.title)}
+                  data-tooltip-id="tip"
+                  data-tooltip-content="Click to add to-do"
+                  data-tooltip-place="left-start"
+                >
+                  {isEditing && editingId === listitem.id ? (
+                    <input
+                      type="text"
+                      className="editInput"
+                      value={editingValue}
+                      onChange={(e) => setEditingValue(e.target.value)}
+                      onKeyDown={handleEditValue}
+                    />
+                  ) : (
+                    <p>{listitem.title}</p>
+                  )}
+                  {isEditing && editingId === listitem.id ? (
+                    <MdPlaylistAdd
+                      className="addItemBtn"
+                      onClick={handleSaveEdit}
+                    />
+                  ) : (
+                    <GrEdit
+                      className="editBtn"
+                      onClick={() => handleEditItem(listitem)}
+                    />
+                  )}
+                  <IoIosClose
+                    className="deleteListItem"
+                    onClick={() => handleRemoveItem(listitem.id)}
+                  />
+                </li>
+              ))}
+            </ul>
+            {messageList && <p className="message">{messageList}</p>}
+          </div>
         </div>
-    );
+      </div>
+      <div className="rghtSec">
+        {selectedListId && (
+          <TodoListItems
+            listId={selectedListId}
+            listTitle={selectedListTitle}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
